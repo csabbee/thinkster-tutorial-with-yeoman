99ac442123f1d71141d2f1a89ac2005ae8dd0f55
Csaba Valyi <csaba_valyi@epam.com>
2014-01-03 10:07:08 +0100

    init commit after yo angular

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('thinksterFirebaseWithYeomanApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
