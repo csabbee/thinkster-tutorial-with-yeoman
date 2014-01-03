5a5471d9e8d98fdf081ea74e2f7b8d2a731947bb
Csaba Valyi <csaba_valyi@epam.com>
2014-01-03 10:53:40 +0100

    firebase added as dependency, new routes registered

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,15 +4,33 @@ angular.module('thinksterFirebaseWithYeomanApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'firebase'
 ])
   .config(function ($routeProvider) {
+    /*global Firebase */
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
+        templateUrl: 'views/default.html',
         controller: 'MainCtrl'
+      })
+      .when('/signin',  {
+        templateUrl: 'views/users/signin.html'
+      })
+      .when('/signup',  {
+        templateUrl: 'views/users/signup.html'
       })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+  //  authentication
+  .run(['$firebaseAuth', 'FBURL', '$rootScope',
+    function($firebaseAuth, FBURL, $rootScope) {
+      $firebaseAuth(new Firebase(FBURL), {
+        path: '/signin'
+      });
+      $rootScope.FBURL = FBURL;
+    }
+  ])
+  .constant('FBURL','https://firefootballtutorial.firebaseio.com/');
